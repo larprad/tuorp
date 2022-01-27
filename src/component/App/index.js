8c4c907d2e15a19e646f61ee3f7ec070f7d7fa39
Loic La Prairie <loic.la.prairie@accenture.com>
2022-01-27 16:46:01 +0100

    Building main components skeleton for the app

--- src/component/App/index.js
+++ src/component/App/index.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Header from "../Header";
+import Grid from "../Grid";
+import Keyboard from "../Keyboard";
 
 function App() {
-  return <div className="App">Hello and welcome to TUORP</div>;
+  return (
+    <div className="min-vh-100 bg-dark text-white">
+      <Header />
+      <Grid />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Header/index.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-header py-3 text-center">
+      <h6 className="text-white fw-bold m-0">TUORP</h6>
+    </div>
+  );
+};
+
+export default Header;
