5af0ec48dabc5c6007cc39eb743cabf928e7ddd9
larprad <59915248+larprad@users.noreply.github.com>
2022-01-30 12:16:32 +0100

    Add words of 5 letters

--- src/config/ini.js
+++ src/config/ini.js
@@ -1,22 +1,53 @@
 export const initGrid = [
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
 ];
 
 export const initKeyboard = [
-  ["A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["Q", "S", "D", "F", "G", "H", "J", "K", "L", "M"],
-  ["VALIDER", "W", "X", "C", "V", "B", "N", "ANNULER"],
+  ['A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  ['Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M'],
+  ['VALIDER', 'W', 'X', 'C', 'V', 'B', 'N', 'ANNULER'],
 ];
 
 export const settings = {
   wordLength: 5,
 };
 
-export const wordToBeFound = "prout"
+export const wordToBeFound = 'prout';
 
-export const authKeys = ["A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P","Q", "S", "D", "F", "G", "H", "J", "K", "L", "M","VALIDER", "W", "X", "C", "V", "B", "N", "ANNULER", "ENTER", "Backspace"]
+export const authKeys = [
+  'A',
+  'Z',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'Q',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'M',
+  'VALIDER',
+  'W',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'ANNULER',
+  'ENTER',
+  'Backspace',
+];
