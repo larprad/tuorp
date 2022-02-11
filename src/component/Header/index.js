c0cac2e15912e5c95913a8eac8b479bab7517eb8
Loic La Prairie <loic.la.prairie@accenture.com>
2022-02-11 18:37:21 +0100

    First shipable version

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -2,8 +2,39 @@ import "./header.scss";
 
 const Header = () => {
   return (
-    <div className="tuorp-header py-4 text-center">
-      <h5 className="text-white fw-bold m-0">TUORP</h5>
+    <div className="tuorp-header py-3 text-center">
+      <h5 className="text-white fw-bold m-0 d-flex justify-content-center">
+        <span
+          className="d-flex align-items-center justify-content-center bg-primary rounded"
+          style={{ width: 40, height: 40 }}
+        >
+          T
+        </span>
+        <span
+          className="d-flex align-items-center justify-content-center rounded"
+          style={{ width: 40, height: 40 }}
+        >
+          U
+        </span>
+        <span
+          className="d-flex align-items-center justify-content-center bg-secondary rounded"
+          style={{ width: 40, height: 40 }}
+        >
+          O
+        </span>
+        <span
+          className="d-flex align-items-center justify-content-center rounded"
+          style={{ width: 40, height: 40 }}
+        >
+          R
+        </span>
+        <span
+          className="d-flex align-items-center justify-content-center rounded"
+          style={{ width: 40, height: 40 }}
+        >
+          P
+        </span>
+      </h5>
     </div>
   );
 };
--- src/component/Key/index.js
+++ src/component/Key/index.js
@@ -1,32 +1,32 @@
-import { useGrid } from '../../hook/useGrid';
-import { useGridContext } from '../../state/GridContext';
-import cn from 'classnames';
-import './key.scss';
+import { useGrid } from "../../hook/useGrid";
+import { useGridContext } from "../../state/GridContext";
+import cn from "classnames";
+import "./key.scss";
 
 const Key = ({ keyCaps }) => {
   const { deleteKey, valideKey, writeKey } = useGrid();
   const { letterFound, letterOut } = useGridContext();
 
   const handleKey = () => {
-    if (keyCaps === 'ANNULER') {
+    if (keyCaps === "ANNULER") {
       deleteKey();
       return;
     }
-    if (keyCaps === 'VALIDER') {
+    if (keyCaps === "VALIDER") {
       valideKey();
       return;
     }
     writeKey(keyCaps);
   };
 
-  const keyClass = cn('tuorp-key p-3 rounded', {
-    'tuorp-key--found': letterFound.includes(keyCaps.toLowerCase()),
-    'tuorp-key--out': letterOut.includes(keyCaps.toLowerCase()),
+  const keyClass = cn("tuorp-key px-sm-3 py-3 px-2 rounded", {
+    "tuorp-key--found": letterFound.includes(keyCaps.toLowerCase()),
+    "tuorp-key--out": letterOut.includes(keyCaps.toLowerCase()),
   });
 
   return (
-    <div className={keyClass} role='button' onClick={handleKey}>
-      <h6 className='m-0 align-middle'>{keyCaps}</h6>
+    <div className={keyClass} role="button" onClick={handleKey}>
+      <h6 className="m-0 align-middle text-white">{keyCaps}</h6>
     </div>
   );
 };
